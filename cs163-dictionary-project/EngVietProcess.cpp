2ea4a92d6abc4fa6cbbbb0173b348264bd0c012f
Gia-Thinh Pham <87513296+pgthinh@users.noreply.github.com>
2022-08-15 15:22:42 +0700

    Update EngVietProcess.cpp
    clean up EngViet(raw)

--- cs163-dictionary-project/EngVietProcess.cpp
+++ cs163-dictionary-project/EngVietProcess.cpp
@@ -1,1 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+	ifstream fin;
+	fin.open("e2.txt");
+	ofstream fout;
+	fout.open("e.txt");
+
+	string s, temp;
+    bool check = false, check1 = false;
+  
+    while (fin){
+        getline(fin,s);
+        if (s[0] == '@'){
+            for (int i = 1; i < s.length(); i++)
+                if (s[i] != ' ' && s[i+1] != '/' && s[i] != ',') //toi phien am thi ngung
+                    fout << s[i];
+                else break;
+            fout << (char) 9;
+            check = true;
+        }
+
+        if (s[0] == '-' && check){
+            for (int i = 2; i < s.length(); i++)
+                fout << s[i];
+            fout << '\n';
+            check = false;
+        }
+	}
+	fin.close();
+	fout.close();
+    return 0;
+}
